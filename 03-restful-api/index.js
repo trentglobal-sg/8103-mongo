7a5ae11ded548d28ffd7da270ab4f2af6dac07c7
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-01-06 08:05:12 +0000

    Addd U and D

--- 03-restful-api/index.js
+++ 03-restful-api/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('dotenv').config();  // put the variables in the .env file into process.env
 const cors = require('cors');
 const { connect } = require("./db");
+const { ObjectId } = require('mongodb');
 
 // SETUP EXPRESS
 const app = express();
@@ -12,6 +13,68 @@ app.use(express.json()); // tell Express that we are sending and reciving JSON
 // SETUP DATABASE
 const mongoUri = process.env.MONGO_URI;
 const dbName = "8103_recipe_book";
+
+async function validateRecipe(db, request) {
+    const { name, cuisine, prepTime, cookTime, servings, ingredients, instructions, tags } = request;
+
+    // basic validation
+    if (!name || !cuisine || !ingredients || !instructions || !tags || !prepTime || !cookTime || !servings) {
+        return {
+            "success": false,
+            "error": "Missing fields",
+        }
+    }
+
+    // validate the cuisine
+    const cuisineDoc = await db.collection('cuisines').findOne({
+        name: cuisine
+    });
+
+    if (!cuisineDoc) {
+        return {
+            "success": false,
+            "error": "Invalid cuisine"
+        }
+    }
+
+    // validate the tags
+
+    // find the tags from the database
+    const tagDocs = await db.collection('tags').find({
+        "name": {
+            $in: tags
+        }
+    }).toArray();
+
+    // check if the number of tags that we have found matches the length of the tags array
+    if (tagDocs.length != tags.length) {
+        return {
+            success: false,
+            error: "One or more tags is invalid"
+        }
+    }
+
+    const newRecipe = {
+        name,
+        cuisine: {
+            _id: cuisineDoc._id,
+            name: cuisineDoc.name
+        },
+        prepTime,
+        cookTime,
+        servings,
+        ingredients,
+        instructions,
+        tags: tagDocs
+    }
+
+    return {
+        success: true,
+        newRecipe: newRecipe,
+        error: null
+    }
+
+}
 
 async function main() {
     const db = await connect(mongoUri, dbName);
@@ -23,13 +86,129 @@ async function main() {
         })
     });
 
-    app.get('/recipes', async function(req,res){
+    app.get('/recipes', async function (req, res) {
         const recipes = await db.collection('recipes').find().project({
             name: 1, cuisine: 1, tags: 1, prepTime: 1
         }).toArray();
         res.json({
             "recipes": recipes
         })
+    })
+
+    // tags: ["quick", "easy", "vegetarian"]
+    app.post('/recipes', async function (req, res) {
+        // extract out the various components of the recipe document from req.body
+        // const name = req.body.name;
+        // const cuisine = req.body.cuisine;
+        // const prepTime = req.body.prepTime;
+        // const cookTime = req.body.cookTime;
+        // const servings = req.body.servings;
+        // const ingredients = req.body.ingredients;
+        // const instructions = req.body.instructions;
+        // const tags = req.body.tags;
+
+        // use object destructuring to extract each components from req.body
+        const { name, cuisine, prepTime, cookTime, servings, ingredients, instructions, tags } = req.body;
+
+        // basic validation
+        if (!name || !cuisine || !ingredients || !instructions || !tags || !prepTime || !cookTime || !servings) {
+            // HTTP 400 error code = Bad request
+            return res.status(400).json({
+                error: "Missing required fields"
+            })
+        }
+
+        // validate the cuisine
+        const cuisineDoc = await db.collection('cuisines').findOne({
+            name: cuisine
+        });
+
+        if (!cuisineDoc) {
+            return res.status(400).json({
+                error: "Error. Cuisine not found"
+            })
+        }
+
+        // validate the tags
+
+        // find the tags from the database
+        const tagDocs = await db.collection('tags').find({
+            "name": {
+                $in: tags
+            }
+        }).toArray();
+
+        // check if the number of tags that we have found matches the length of the tags array
+        if (tagDocs.length != tags.length) {
+            return res.status(400).json({
+                'error': "One or more tags is invalid"
+            })
+        }
+
+        const newRecipe = {
+            _id: new ObjectId(),  // optional, 'cos when Mongo inserts a new document, it will ensure that an _id
+            name,
+            cuisine: {
+                _id: cuisineDoc._id,
+                name: cuisineDoc.name
+            },
+            prepTime,
+            cookTime,
+            servings,
+            ingredients,
+            instructions,
+            tags: tagDocs
+        }
+
+        const result = await db.collection('recipes').insertOne(newRecipe);
+        res.status(201).json({
+            message: "Recipe created",
+            recipeId: result.insertedId
+        })
+    })
+
+    app.put('/recipes/:id', async function (req, res) {
+        const recipeId = req.params.id;
+        const status = await validateRecipe(db, req.body);
+        if (status.success) {
+            // update the recipe
+            const result = await db.collection('recipes').updateOne({
+                _id: new ObjectId(recipeId)
+            }, {
+                $set: status.newRecipe
+            });
+            res.json({
+                'message': "Recipe has been updated successful"
+            })
+        } else {
+            res.status(400).json({
+                error: status.error
+            })
+        }
+    })
+
+    app.delete('/recipes/:id', async function (req, res) {
+        try {
+            const recipeId = req.params.id;
+            const results = await db.collection('recipes').deleteOne({
+                _id: new ObjectId(recipeId)
+            });
+
+            if (results.deletedCount === 0) {
+                return res.status(404).json({
+                    "error":"Not found"
+                })
+            }
+
+            res.json({
+                'message':'Deleted successfully'
+            })
+        } catch (e) {
+            res.status(500).json({
+                'error':'Internal Server Error'
+            })
+        }
+
     })
 }
 main();
