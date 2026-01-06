7097b956f02af996426becdb5e3b11d6ac2254ef
trentglobal-sg <paul.chor@trentglobal.edu.sg>
2026-01-06 06:16:09 +0000

    Added GET route

--- 03-restful-api/index.js
+++ 03-restful-api/index.js
@@ -2,18 +2,41 @@
 const express = require('express');
 require('dotenv').config();  // put the variables in the .env file into process.env
 const cors = require('cors');
+const { connect } = require("./db");
 
 // SETUP EXPRESS
 const app = express();
 app.use(cors()); // enable CORS for API
 app.use(express.json()); // tell Express that we are sending and reciving JSON
 
-// ROUTES
-app.get('/test', function(req,res){
-    res.send("It's alive");
-})
+// SETUP DATABASE
+const mongoUri = process.env.MONGO_URI;
+const dbName = "8103_recipe_book";
+
+async function main() {
+    const db = await connect(mongoUri, dbName);
+
+    // ROUTES
+    app.get('/test', function (req, res) {
+        res.json({
+            "message": "Hello world"
+        })
+    });
+
+    app.get('/recipes', async function(req,res){
+        const recipes = await db.collection('recipes').find().project({
+            name: 1, cuisine: 1, tags: 1, prepTime: 1
+        }).toArray();
+        res.json({
+            "recipes": recipes
+        })
+    })
+}
+main();
+
+
 
 // START SERVER
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server has started");
 })
